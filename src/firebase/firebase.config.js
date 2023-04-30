eba5089a8d5325e570bea38af6b867e8902dab30
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-04-30 23:42:25 +0600

    hidden firebase config with env variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRBNpMaCK1W6ZLtwlN0fTHvzOWIyNJPGY",
-  authDomain: "travel-guru-e65e5.firebaseapp.com",
-  projectId: "travel-guru-e65e5",
-  storageBucket: "travel-guru-e65e5.appspot.com",
-  messagingSenderId: "454722089383",
-  appId: "1:454722089383:web:0fb990ae91cfb1d21b4682",
+  apiKey: import.meta.envVITE_APIKEY,
+  authDomain: import.meta.envVITE_AUTHDOMAIN,
+  projectId: import.meta.envVITE_PROJECTID,
+  storageBucket: import.meta.envVITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.envVITE_MESSAGINGSENDERID,
+  appId: import.meta.envVITE_APPID,
 };
 
 // Initialize Firebase
